28986d1616c81ffbbb325779abcfe62e54ae2b09
Kiyoonewton <kiyoonewton41@gmail.com>
2025-11-12 10:09:07 +0100

    feat: add setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Counter from "@/shared/components/Counter";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <Counter />
+    </main>
+  );
+}
