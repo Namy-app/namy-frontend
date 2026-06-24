4cd5460e4a6e59eba35ee459fc9ea022ff22d5a5
isaac-hope <isaac@hopeplatforms.com>
2026-06-24 20:24:24 +0100

    add firebase to the project

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -24,6 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
         NotificationCenter.default.post(name: .capacitorDidRegisterForRemoteNotifications, object: deviceToken)
     }
 
